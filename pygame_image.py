4319715b69f100cb788ecb71b22528de9b676e65
c0a23129 <c0a231296c@edu.teu.ac.jp>
2024-11-19 15:38:20 +0900

    練習2こうかとん画像の描画

--- pygame_image.py
+++ pygame_image.py
@@ -10,12 +10,15 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kokaton_fry_img = pg.image.load("fig/3.png")#こうかとんのsurfaceを生成
+    kokaton_fry_img = pg.transform.flip(kokaton_fry_img, True, False)#画像を左右反転
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])#screen surfaceに背景画像を張りつける
+        screen.blit(kokaton_fry_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
