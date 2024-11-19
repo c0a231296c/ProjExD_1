71d80399a30ed9be88b660534daf706ad6f7e390
c0a23129 <c0a231296c@edu.teu.ac.jp>
2024-11-19 15:48:09 +0900

    練習６背景画像が動く

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,9 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])#screen surfaceに背景画像を張りつける
+        if tmr >= 1600 - 800:
+            tmr = 0
+        screen.blit(bg_img, [-tmr, 0])#screen surfaceに背景画像を張りつける
         screen.blit(kokaton_fry_img, [300, 200])
         pg.display.update()
         tmr += 1        
